01a566f79e124b10e4b13e8ba7d93026aa123ccd
Shawn Kawabe <59843897+shawnkawabe@users.noreply.github.com>
2020-03-03 00:49:45 -0300

    Delete screen.py

--- screen.py
+++
@@ -1,6 +0,0 @@
-import ctypes
-def gets():
-    user32 = ctypes.windll.user32
-    screenx = str(user32.GetSystemMetrics(0))
-    screeny = str(user32.GetSystemMetrics(1))
-    return(screenx+"x"+screeny)
