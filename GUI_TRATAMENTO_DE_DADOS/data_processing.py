fe2b65c58ef1a433475630c243a3f185fbd265b9
Shawn Kawabe <59843897+shawnkawabe@users.noreply.github.com>
2020-03-03 00:50:37 -0300

    VER. 0.14

--- GUI_TRATAMENTO_DE_DADOS/data_processing.py
+++ GUI_TRATAMENTO_DE_DADOS/data_processing.py
@@ -1,3 +1,6 @@
+#import function(classes,def)
+import CreateList from List
+#import packages
 import os 
 
 class dataprocess():
@@ -15,8 +18,38 @@ class dataprocess():
         document.close()
         
     def process(self):
-        print(self.document)
-        print(self.name)
-        print(self.numberlines)
-        print(self.lines)
-        input()
+        doc = self.document
+		docp = ""
+		docl = []
+		CreateList.newList(self.name,self.numberlines)
+
+	
+for i in range(len(doc)):
+    docp+=doc[i]
+    
+for i in range(len(doc)):
+    docl.append(doc[i].replace('\n','').replace(' ','').split('\t'))
+    
+for i in range(len(docl)):
+    for d in range(len(docl[i])):
+        docl[i][d] = docl[i][d].replace(',','.')
+docl.pop(-1)
+docl.pop(-1)
+
+c = 1
+
+dt = open('t.dat','w')#Criando os arquivos para cada dado.
+dx = open('x.dat','w')
+dy = open('y.dat','w')
+print(doc[c][0])
+while c<len(docl):
+    t.append(docl[c][0])
+    x.append(docl[c][1])
+    y.append(docl[c][2])
+    dt.write(docl[c][0]+'\n')#Escrevendo a linha especifica no arquivo.
+    dx.write(docl[c][1]+'\n')
+    dy.write(docl[c][2]+'\n')
+    c+=1
+dt.close()#Fechando arquivo.
+dx.close()
+dy.close()    
